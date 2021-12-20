743c81f9c63ae48b7011b5d87f6128f9901d0c1a
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-20 13:05:29 -0500

    Built Middleware: validateLogin

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,7 @@
 const Users = require('../users/users-model');
 const { lowerCase } = require('lower-case');
 const bcrypt = require('bcryptjs');
+const tokenBuilder = require('./token-builder');
 
 // Check for empty fields
 const validateEmptyFields = (req, res, next) => {
@@ -45,8 +46,29 @@ const hashPassword = (req, res, next) => {
   next();
 };
 
+// Login validations
+const validateLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await Users.getBy({ username: lowerCase(username) });
+  try {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = tokenBuilder(user);
+      req.token = token;
+      next();
+    } else {
+      next({
+        status: 401,
+        message: 'Invalid Credentials',
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   validateEmptyFields,
   validateUsername,
   hashPassword,
+  validateLogin,
 };
