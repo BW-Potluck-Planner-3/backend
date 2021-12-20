15ad93b260552cd14287d6c2953192ebfb36641a
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-20 13:00:49 -0500

    Built Middleware: validateEmptyFields, validateUsername, hashPassword

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,1 +1,52 @@
-// WIP
+const Users = require('../users/users-model');
+const { lowerCase } = require('lower-case');
+const bcrypt = require('bcryptjs');
+
+// Check for empty fields
+const validateEmptyFields = (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    next({
+      status: 400,
+      message: 'username and password required',
+    });
+  } else {
+    next();
+  }
+};
+
+// Check for existing username
+const validateUsername = async (req, res, next) => {
+  try {
+    const user = await Users.getBy({ username: lowerCase(req.body.username) });
+    if (user) {
+      next({
+        status: 401,
+        message: 'username already exists',
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Hash password
+const hashPassword = (req, res, next) => {
+  const { username, password } = req.body;
+  const rounds = process.env.BCRYPT_ROUNDS || 8;
+  const hash = bcrypt.hashSync(password, rounds);
+
+  req.body = {
+    username,
+    password: hash,
+  };
+  next();
+};
+
+module.exports = {
+  validateEmptyFields,
+  validateUsername,
+  hashPassword,
+};
