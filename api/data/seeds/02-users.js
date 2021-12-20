d91281de5a9970523e5a8eb7c4a5a58ca82aee61
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-20 10:43:43 -0800

    Created users seed and added new config in secrets

---
+++ api/data/seeds/02-users.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcryptjs")
+const { BCRYPT_ROUNDS } = require("../../secrets")
+
+const users = [
+  { username: "sam", password: bcrypt.hashSync("1234", BCRYPT_ROUNDS) },
+  { username: "frodo", password: bcrypt.hashSync("1234", BCRYPT_ROUNDS) },
+  { username: "pippin", password: bcrypt.hashSync("1234", BCRYPT_ROUNDS) },
+  { username: "merry", password: bcrypt.hashSync("1234", BCRYPT_ROUNDS) },
+]
+
+exports.seed = function (knex) {
+  return knex("users").insert(users)
+}
