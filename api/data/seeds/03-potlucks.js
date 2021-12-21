389a2433cda1acee1df1356369552c4ace5e10ba
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-21 09:47:25 -0800

    Seeded two potlucks into the database

--- api/data/seeds/03-potlucks.js
+++ api/data/seeds/03-potlucks.js
@@ -1,18 +1,18 @@
 const potluckDates = [
-  { potluck1: new Date(Date.UTC(2022, 2, 3, 13, 30)) },
-  { potluck2: new Date(Date.UTC(2022, 0, 12, 14, 30)) }
+  new Date(Date.UTC(2022, 2, 3, 13, 30)),
+  new Date(Date.UTC(2022, 1, 12, 14, 30))
 ]
 
 function getDate(date) { // pulls year, month, and day from JS date
-  const year = date.getFullYear()
-  const month = date.getMonth()
-  const day = date.getDate()
+  const year = date.getUTCFullYear()
+  const month = date.getUTCMonth()
+  const day = date.getUTCDate()
   return `${month}/${day}/${year}`
 }
 
 function getTime(date) { // pulls hour and minute from JS date
-  let hour = date.getHours()
-  const minute = date.getMinutes()
+  let hour = date.getUTCHours()
+  const minute = date.getUTCMinutes()
   if (hour > 12) {
     hour -= 12
     return `${hour}:${minute} PM UTC`
@@ -22,8 +22,18 @@ function getTime(date) { // pulls hour and minute from JS date
 
 const potlucks = [
   {
-    potluck_name: "",
-
+    potluck_name: "Frodo's Farewell Party",
+    date: getDate(potluckDates[0]),
+    time: getTime(potluckDates[0]),
+    location: "The Shire",
+    user_id: 2
+  },
+  {
+    potluck_name: "Pippin's Great Big Feast",
+    date: getDate(potluckDates[1]),
+    time: getTime(potluckDates[1]),
+    location: "The Shire",
+    user_id: 3
   },
 ]
 
