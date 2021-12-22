00646a250bec76a3baf2b7b05298e573ee84a44e
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-22 14:31:44 -0800

    Updated guests seed

--- api/data/seeds/06-guests.js
+++ api/data/seeds/06-guests.js
@@ -1,13 +1,11 @@
 const guests = [
   // Potluck 1
   { food_potluck_id: 1, potluck_id: 1, user_id: 1, attending: true },
-  { food_potluck_id: 1, potluck_id: 1, user_id: 2, attending: false },
   { food_potluck_id: 2, potluck_id: 1, user_id: 3, attending: true },
-  { food_potluck_id: 3, potluck_id: 1, user_id: 4, attending: true },
+  { food_potluck_id: 3, potluck_id: 1, user_id: 4, attending: false },
   // Potluck 2
-  { food_potluck_id: 2, potluck_id: 2, user_id: 1, attending: true },
-  { food_potluck_id: 4, potluck_id: 2, user_id: 2, attending: true },
-  { food_potluck_id: 5, potluck_id: 2, user_id: 3, attending: false },
+  { food_potluck_id: 5, potluck_id: 2, user_id: 1, attending: true },
+  { food_potluck_id: 4, potluck_id: 2, user_id: 2, attending: false },
   { food_potluck_id: 6, potluck_id: 2, user_id: 4, attending: true },
 ]
 
