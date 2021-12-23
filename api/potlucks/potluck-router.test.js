6fefd3bdaf1f0927e779db7978f9d04c6b4d4334
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-22 18:10:37 -0800

    Full test coverage on post guest potluck endpoint

--- api/potlucks/potluck-router.test.js
+++ api/potlucks/potluck-router.test.js
@@ -434,4 +434,43 @@ describe("[POST] - /api/potlucks/:potluck_id/guests", () => {
       })
     })
   })
+
+  describe("requesting with no token", () => {
+    let res
+    beforeAll(async () => {
+      res = await request(server)
+        .post("/api/potlucks/3/guests")
+        .send({
+          user_id: 4,
+          attending: true
+        })
+    })
+    it("responds with the message 'token required'", () => {
+      const expected = /token required/i
+      expect(res.body.message).toMatch(expected)
+    })
+    it("responds with the status code 401", () => {
+      expect(res.status).toBe(401)
+    })
+  })
+
+  describe("requesting with an invalid token", () => {
+    let res
+    beforeAll(async () => {
+      res = await request(server)
+        .post("/api/potlucks/3/guests")
+        .send({
+          user_id: 1,
+          attending: true
+        })
+        .set("Authorization", "ultraJunk")
+    })
+    it("responds with the message 'invalid token'", () => {
+      const expected = /invalid token/i
+      expect(res.body.message).toMatch(expected)
+    })
+    it("responds with the status code 401", () => {
+      expect(res.status).toBe(401)
+    })
+  })
 })
