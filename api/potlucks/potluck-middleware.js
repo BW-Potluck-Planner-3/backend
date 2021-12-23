9b070232026ee4ca5ce7e69973b947efabd84cfe
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-23 10:35:41 -0500

    Created validatePotluckPayload middleware

--- api/potlucks/potluck-middleware.js
+++ api/potlucks/potluck-middleware.js
@@ -1,5 +1,5 @@
 const Potlucks = require('./potlucks-model');
-const Users = require("../users/users-model")
+const Users = require('../users/users-model');
 
 const checkPotluckId = async (req, res, next) => {
   try {
@@ -18,36 +18,55 @@ const checkPotluckId = async (req, res, next) => {
   }
 };
 
+const validatePotluckPayload = async (req, res, next) => {
+  try {
+    const required = ['potluck_name', 'date', 'time', 'location'];
+    if (!required.every((field) => req.body[field])) {
+      next({
+        status: 400,
+        message: `${required
+          .filter((field) => !req.body[field])
+          .join(', ')} is missing`,
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
 async function validateAddGuestPayload(req, res, next) {
-  const { user_id, attending } = req.body
-  if (!user_id || !attending || typeof attending !== "boolean") {
+  const { user_id, attending } = req.body;
+  if (!user_id || !attending || typeof attending !== 'boolean') {
     return next({
       status: 400,
-      message: "Please provide a user_id and an attending boolean"
-    })
+      message: 'Please provide a user_id and an attending boolean',
+    });
   }
-  const existingUser = await Users.getById(user_id)
+  const existingUser = await Users.getById(user_id);
   if (!existingUser) {
     return next({
       status: 404,
-      message: `User with id of ${user_id} does not exist`
-    })
+      message: `User with id of ${user_id} does not exist`,
+    });
   }
-  const potluck = await Potlucks.getById(req.params.potluck_id)
+  const potluck = await Potlucks.getById(req.params.potluck_id);
   if (potluck.user_id === user_id) {
     return next({
       status: 400,
-      message: "Cannot add a potluck organizer as their own guest"
-    })
+      message: 'Cannot add a potluck organizer as their own guest',
+    });
   }
   req.body.guest = {
     user_id: existingUser.user_id,
-    attending: req.body.attending
-  }
-  return next()
+    attending: req.body.attending,
+  };
+  return next();
 }
 
 module.exports = {
   checkPotluckId,
-  validateAddGuestPayload
+  validatePotluckPayload,
+  validateAddGuestPayload,
 };
